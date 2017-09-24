fc8775503ed76833b60481cb2e0e69c62e1c97a9
Arthur Diaz <adiaz@csumb.edu>
2017-09-24 19:53:21 +0000

    changes

---
+++ practice/Sept20Notes.php
@@ -0,0 +1,27 @@
+
+
+        
+<?php
+
+//$players = array("Juan" =>20);
+//$players["John"] = 50;
+
+$_SESSION['Juan'] = 40;
+
+echo $players["John"];
+echo "<br />";
+echo $_SESSION["Juan"]; //look up what session is to make sure you understand.
+?>
+
+
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> </title>
+    </head>
+    <body>
+
+
+    </body>
+</html>
