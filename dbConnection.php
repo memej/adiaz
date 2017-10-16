d9f5b86285d074dc3253aa81788ad2ebaf7adf9f
Arthur Diaz <adiaz@csumb.edu>
2017-10-16 02:04:53 +0000

    help

--- dbConnection.php
+++ dbConnection.php
@@ -10,7 +10,7 @@ function getDatabaseConnection(){
     
     //using different database variables in Heroku
     if  (strpos($_SERVER['HTTP_HOST'], 'herokuapp') !== false) {
-        $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+        $url = parse_url(getenv(CLEARDB_DATABASE_URL));
         $host = $url["host"];
         $dbname = substr($url["path"], 1);
         $username = $url["user"];
