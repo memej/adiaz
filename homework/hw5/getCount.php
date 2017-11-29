6fcf54dc04bb8c23a48787db51dbd87b702cda2d
Arthur Diaz <adiaz@csumb.edu>
2017-11-29 21:10:16 +0000

    works

--- homework/hw5/getCount.php
+++ homework/hw5/getCount.php
@@ -2,17 +2,12 @@
 include '../../dbConnection.php';
 $conn = getDatabaseConnection();
 $value=$_GET['passValue'];
- $sql="SELECT * 
-          FROM walmartapi
-          WHERE search='$value'";
+ $sql="SELECT COUNT(*) AS c FROM `walmartapi` WHERE search='$value' GROUP BY search";
+    
+          
   $stmt=$conn->prepare($sql);
   $stmt->execute();
-  $records=$stmt->fetchAll();
-  $count = 0;
-  foreach($records as $record){
-        $count++;
-     
-  }
-  
-  echo $count;
+  $records=$stmt->fetch(PDO::FETCH_ASSOC);
+ 
+  echo $records['c'];
 ?>
